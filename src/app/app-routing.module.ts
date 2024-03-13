42cc221243b2aca4566c22904ad91ebf317c0acd
_bsantos <bsantos@simetricaconsulting.com>
2024-03-13 03:46:00 -0400

    estructura del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { CertificadoComponent } from './components/certificado/certificado.component';
+import { AuthService } from './services/auth.service';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'certificate', component: CertificadoComponent, canActivate: [AuthService] },
+  { path: 'solicitudes', component: SolicitudesComponent, canActivate: [AuthService], data: { roles: ['administrative'] } },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CertificadoComponent } from './certificado/certificado.component';
+import { CertificadoComponent } from './components/certificado/certificado.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { ObtenerCertificadoComponent } from './components/obtener-certificado/obtener-certificado.component';
+import { RequisitosComponent } from './components/requisitos/requisitos.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CertificadoComponent
+    CertificadoComponent,
+    LoginComponent,
+    ObtenerCertificadoComponent,
+    RequisitosComponent,
+    SolicitudesComponent
   ],
   imports: [
     BrowserModule,
